607ef363d8b2102e6c55fe9b98ba5c772fe8e85b
chakio <y-kawasaki@keio.jp>
2019-08-15 16:14:35 +0900

    add rviz

--- docker_ros.sh
+++
@@ -1,1 +0,0 @@
-docker run -it --rm  -v "/home/$(whoami)/catkin_ws/:/catkin_ws/" --net host ros:ros-tutorials
---
+++ ros_tutorial/run.sh
@@ -0,0 +1,11 @@
+docker run -it \
+--env=DISPLAY=$DISPLAY\
+--volume="/etc/group:/etc/group:ro"\
+--volume="/etc/passwd:/etc/passwd:ro" \
+--volume="/etc/shadow:/etc/shadow:ro" \
+--volume="/etc/sudoers.d:/etc/sudoers.d:ro" \
+--volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+--env=QT_X11_NO_MITSHM=1\
+--rm \
+-v "/home/$(whoami)/catkin_ws/:/catkin_ws/" \
+--net host ros:ros-tutorials

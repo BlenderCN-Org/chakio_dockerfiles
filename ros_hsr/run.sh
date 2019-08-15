80b9862027671002a0738d0420efe4d47638f1e7
chakio <y-kawasaki@keio.jp>
2019-08-15 20:10:43 +0900

    add ros_hsr

---
+++ ros_hsr/run.sh
@@ -0,0 +1,10 @@
+xhost +local:user
+docker run -it \
+--runtime=nvidia \
+--env=DISPLAY=$DISPLAY \
+--volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+--env="QT_X11_NO_MITSHM=1" \
+--rm \
+-v "/home/$(whoami)/catkin_ws/:/catkin_ws/" \
+--net host \
+ros:ros-hsr \

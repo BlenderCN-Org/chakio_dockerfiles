fef0b5a6145309b4498d382468c2e71d719c829b
chakio <y-kawasaki@keio.jp>
2019-08-24 21:19:14 +0900

    edit realsense

--- realsense/startup.sh
+++ realsense/startup.sh
@@ -5,7 +5,7 @@ source /home/catkin_ws/devel/setup.bash && \
 #export ROS_TP=${CLIENT_IP} && \
 #roslaunch realsense2_camera rs_rgbd.launch align_depth:=true camera:=rs01
 #roslaunch realsense2_camera rs_rgbd.launch align_depth:=true camera:=rs02
-roslaunch realsense2_camera rs_multiple_cameras.launch align_depth:=true camera1:=rs01 serial_no_camera1:=838212070676  camera2:=rs02 serial_no_camera2:=838212072328
+roslaunch realsense2_camera rs_multiple_cameras.launch align_depth:=true camera1:=rs00 serial_no_camera1:=838212070676  camera2:=rs01 serial_no_camera2:=838212072328
 #roslaunch realsense2_camera rs_rgbd.launch align_depth:=true camera1:=rs01 serial_no_camera1:=838212070676
 #roslaunch realsense2_camera rs_rgbd.launch align_depth:=true camera:=rs02
  

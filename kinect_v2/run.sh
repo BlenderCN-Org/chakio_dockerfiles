8982b1885f36f36778ea6772e7677200d5735f5b
chakio <y-kawasaki@keio.jp>
2019-08-30 17:46:01 +0900

    add kinect

---
+++ kinect_v2/run.sh
@@ -0,0 +1,13 @@
+xhost +local:user
+docker run -it \
+--runtime=nvidia \
+--env=DISPLAY=$DISPLAY \
+--volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+--env="QT_X11_NO_MITSHM=1" \
+--rm \
+-v "/$(pwd)/ros_setting.sh:/ros_setting.sh" \
+-v /etc/group:/etc/group:ro \
+-v /etc/passwd:/etc/passwd:ro \
+--net host \
+--privileged \
+ros:ros-kinect-v2 \

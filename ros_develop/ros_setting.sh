0ec0c6fee5ddaa186d499a7959da2a6c5c3ccd2e
chakio <y-kawasaki@keio.jp>
2019-08-30 11:21:51 +0900

    change file constraction

--- ros_develop/ros_setting.sh
+++ ros_develop/ros_setting.sh
@@ -1,6 +1,7 @@
 export ROS_HOME=~/.ros
 source /catkin_ws/devel/setup.bash
-network_if=enp60s0
+#network_if=enp60s0
+network_if=enp4s0
 export TARGET_IP=$(LANG=C /sbin/ifconfig $network_if | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*')
 if [ -z "$TARGET_IP" ] ; then
     echo "ROS_IP is not set."
---
+++ xtion_pro_live/build.sh
@@ -0,0 +1,1 @@
+docker build --tag ros:ros-xtion-pro-live  .

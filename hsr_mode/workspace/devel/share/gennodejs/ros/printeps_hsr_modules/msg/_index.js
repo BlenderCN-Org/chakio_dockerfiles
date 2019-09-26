20c4450154a60b940106287f5437b98075278ca8
chakio <kawasakiyousuke1@gmail.com>
2019-09-26 15:27:52 +0900

    add printeps hsr modules

--- hsr_mode/workspace/devel/share/gennodejs/ros/printeps_hsr_modules/msg/_index.js
+++ hsr_mode/workspace/devel/share/gennodejs/ros/printeps_hsr_modules/msg/_index.js
@@ -1,16 +1,16 @@
 
 "use strict";
 
-let HsrPerson = require('./HsrPerson.js');
-let HsrOpenpose = require('./HsrOpenpose.js');
-let HsrObstacleArray = require('./HsrObstacleArray.js');
 let HsrObstacle = require('./HsrObstacle.js');
+let HsrPerson = require('./HsrPerson.js');
+let HsrObstacleArray = require('./HsrObstacleArray.js');
 let HsrTableset = require('./HsrTableset.js');
+let HsrOpenpose = require('./HsrOpenpose.js');
 
 module.exports = {
-  HsrPerson: HsrPerson,
-  HsrOpenpose: HsrOpenpose,
-  HsrObstacleArray: HsrObstacleArray,
   HsrObstacle: HsrObstacle,
+  HsrPerson: HsrPerson,
+  HsrObstacleArray: HsrObstacleArray,
   HsrTableset: HsrTableset,
+  HsrOpenpose: HsrOpenpose,
 };
--- hsr_mode/workspace/devel/share/gennodejs/ros/printeps_hsr_modules/srv/_index.js
+++ hsr_mode/workspace/devel/share/gennodejs/ros/printeps_hsr_modules/srv/_index.js
@@ -1,46 +1,46 @@
 
 "use strict";
 
-let HsrHandRelease = require('./HsrHandRelease.js')
-let HsrPickUpObject = require('./HsrPickUpObject.js')
-let HsrNav = require('./HsrNav.js')
 let PathGenerator = require('./PathGenerator.js')
-let HsrCarry2017 = require('./HsrCarry2017.js')
-let HsrCarry = require('./HsrCarry.js')
-let HsrLog = require('./HsrLog.js')
-let HsrReleaseObject = require('./HsrReleaseObject.js')
-let HsrGlbNav = require('./HsrGlbNav.js')
-let HsrSymNav = require('./HsrSymNav.js')
-let HsrSay = require('./HsrSay.js')
-let HsrPlaceObject = require('./HsrPlaceObject.js')
-let HsrHoldObject = require('./HsrHoldObject.js')
-let HsrLogP = require('./HsrLogP.js')
-let Float32ToPose2D = require('./Float32ToPose2D.js')
-let HsrMMNav = require('./HsrMMNav.js')
-let HsrNav3 = require('./HsrNav3.js')
 let HsrNav2 = require('./HsrNav2.js')
-let HsrPickAndPlace = require('./HsrPickAndPlace.js')
+let HsrGlbNav = require('./HsrGlbNav.js')
+let HsrNav3 = require('./HsrNav3.js')
+let HsrHoldObject = require('./HsrHoldObject.js')
+let HsrSay = require('./HsrSay.js')
+let HsrReleaseObject = require('./HsrReleaseObject.js')
+let HsrCarry = require('./HsrCarry.js')
+let HsrSymNav = require('./HsrSymNav.js')
+let HsrPickUpObject = require('./HsrPickUpObject.js')
+let HsrPlaceObject = require('./HsrPlaceObject.js')
+let Float32ToPose2D = require('./Float32ToPose2D.js')
 let HsrHandoverObject = require('./HsrHandoverObject.js')
+let HsrHandRelease = require('./HsrHandRelease.js')
+let HsrMMNav = require('./HsrMMNav.js')
+let HsrLogP = require('./HsrLogP.js')
+let HsrNav = require('./HsrNav.js')
+let HsrLog = require('./HsrLog.js')
+let HsrCarry2017 = require('./HsrCarry2017.js')
+let HsrPickAndPlace = require('./HsrPickAndPlace.js')
 
 module.exports = {
-  HsrHandRelease: HsrHandRelease,
-  HsrPickUpObject: HsrPickUpObject,
-  HsrNav: HsrNav,
   PathGenerator: PathGenerator,
-  HsrCarry2017: HsrCarry2017,
-  HsrCarry: HsrCarry,
-  HsrLog: HsrLog,
-  HsrReleaseObject: HsrReleaseObject,
-  HsrGlbNav: HsrGlbNav,
-  HsrSymNav: HsrSymNav,
-  HsrSay: HsrSay,
-  HsrPlaceObject: HsrPlaceObject,
-  HsrHoldObject: HsrHoldObject,
-  HsrLogP: HsrLogP,
-  Float32ToPose2D: Float32ToPose2D,
-  HsrMMNav: HsrMMNav,
-  HsrNav3: HsrNav3,
   HsrNav2: HsrNav2,
-  HsrPickAndPlace: HsrPickAndPlace,
+  HsrGlbNav: HsrGlbNav,
+  HsrNav3: HsrNav3,
+  HsrHoldObject: HsrHoldObject,
+  HsrSay: HsrSay,
+  HsrReleaseObject: HsrReleaseObject,
+  HsrCarry: HsrCarry,
+  HsrSymNav: HsrSymNav,
+  HsrPickUpObject: HsrPickUpObject,
+  HsrPlaceObject: HsrPlaceObject,
+  Float32ToPose2D: Float32ToPose2D,
   HsrHandoverObject: HsrHandoverObject,
+  HsrHandRelease: HsrHandRelease,
+  HsrMMNav: HsrMMNav,
+  HsrLogP: HsrLogP,
+  HsrNav: HsrNav,
+  HsrLog: HsrLog,
+  HsrCarry2017: HsrCarry2017,
+  HsrPickAndPlace: HsrPickAndPlace,
 };
